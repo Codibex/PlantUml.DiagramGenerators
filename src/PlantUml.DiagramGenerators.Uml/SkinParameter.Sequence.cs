99a1f5900a7b1bf6844247cdd7ec356ad88b0920
Michael Huemer <Michael_H@LIVE.AT>
2022-10-24 12:30:40 +0200

    Move to partial class

---
+++ src/PlantUml.DiagramGenerators.Uml/SkinParameter.Sequence.cs
@@ -0,0 +1,20 @@
+﻿namespace PlantUml.DiagramGenerators.Uml;
+public partial class SkinParameter
+{
+    public static SkinParameter SequenceMessageAlignment(SequenceMessageAlignment messageAlignment)
+    {
+        string alignment = messageAlignment switch
+        {
+            Uml.SequenceMessageAlignment.Left => "left",
+            Uml.SequenceMessageAlignment.Right => "right",
+            _ => throw new ArgumentOutOfRangeException(nameof(messageAlignment), messageAlignment, null)
+        };
+
+        return new SkinParameter($"sequenceMessageAlign {alignment}");
+    }
+
+    public static SkinParameter ResponseMessageBelowArrow(bool value)
+    {
+        return new SkinParameter($"responseMessageBelowArrow {value.ToString().ToLower()}");
+    }
+}
--- src/PlantUml.DiagramGenerators.Uml/SkinParameter.cs
+++ src/PlantUml.DiagramGenerators.Uml/SkinParameter.cs
@@ -1,5 +1,6 @@
 ﻿namespace PlantUml.DiagramGenerators.Uml;
-public class SkinParameter
+
+public partial class SkinParameter
 {
     private string Parameter { get; }
 
@@ -13,23 +14,6 @@ public class SkinParameter
 
     public static SkinParameter ClassFontColor(string color) 
         => new($"classFontColor {color}");
-
-    public static SkinParameter SequenceMessageAlignment(SequenceMessageAlignment messageAlignment)
-    {
-        string alignment = messageAlignment switch
-        {
-            Uml.SequenceMessageAlignment.Left => "left",
-            Uml.SequenceMessageAlignment.Right => "right",
-            _ => throw new ArgumentOutOfRangeException(nameof(messageAlignment), messageAlignment, null)
-        };
-        
-        return new SkinParameter($"sequenceMessageAlign {alignment}");
-    }
-
-    public static SkinParameter ResponseMessageBelowArrow(bool value)
-    {
-        return new SkinParameter($"responseMessageBelowArrow {value.ToString().ToLower()}");
-    }
 
     public static implicit operator string(SkinParameter parameter) => parameter.GetStatement();
 
